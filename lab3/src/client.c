5268789a49d371971ff78567fa7be6fc4cd8092b
axywe <axywewastaken@gmail.com>
2024-04-11 02:49:02 +0300

    lab3 server

--- lab3/src/client.c
+++ lab3/src/client.c
@@ -1,11 +1,3 @@
-/**
- * @file client.c
- * @brief Client application for connecting to the guessing game server over TCP/IP.
- *
- * This client application connects to a specified server running the guessing game,
- * sends user guesses, and receives responses from the server. The game continues until
- * the user either guesses the correct number or chooses to exit.
- */
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdbool.h>
@@ -15,23 +7,48 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 8080 /**< The port number to connect to the guessing game server on. */
-#define BUFFER_SIZE 1024 /**< Buffer size for receiving messages from the server. */
+#define PORT 8080
+#define BUFFER_SIZE 256
 
-
-/**
- * @brief Main function that establishes connection to the server and processes user input.
- * 
- * Connects to the game server, handles the sending of user guesses, and processes
- * responses from the server. Continues until the user wins, loses, or exits.
- *
- * @return int Returns 0 on successful execution, or -1 on error.
- */
-int main() {
+int main(int argc, char *argv[]) {
   int sock = 0, valread;
   struct sockaddr_in serv_addr;
   char buffer[BUFFER_SIZE] = {0};
   char input[BUFFER_SIZE];
+
+  char *host = NULL;
+  int port = 0;
+  char *name = NULL;
+
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-h") == 0) {
+      host = argv[i + 1];
+    } else if (strcmp(argv[i], "-p") == 0) {
+      port = atoi(argv[i + 1]);
+    } else if (strcmp(argv[i], "-n") == 0) {
+      name = argv[i + 1];
+      name[strlen(name)] = '\0';
+    }
+  }
+
+  if (host == NULL) {
+    host = "127.0.0.1";
+  }
+  if (port == 0) {
+    port = 8080;
+  }
+  if (name == NULL) {
+    printf("Usage: %s -h <host> -p <port> -n <name>\n", argv[0]);
+    return -1;
+  }
+  if (port < 1024 || port > 65535) {
+    printf("Port must be in range 1024-65535\n");
+    return -1;
+  }
+  if (strlen(name) > 255) {
+    printf("Name must be less than 255 characters\n");
+    return -1;
+  }
 
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Socket creation error \n");
@@ -41,7 +58,7 @@ int main() {
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
 
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0) {
     printf("\nInvalid address/ Address not supported \n");
     return -1;
   }
@@ -51,26 +68,41 @@ int main() {
     return -1;
   }
 
+  send(sock, name, strlen(name), 0);
   valread = read(sock, buffer, BUFFER_SIZE);
-  printf("%s\n", buffer); 
+  int min, max, attempts;
+  if (valread == 0) {
+    printf("Server disconnected\n");
+    return -1;
+  } else if (buffer[0] == 'h') {
+    if(sscanf(buffer, "h %d %d %d", &min, &max, &attempts) == 3){
+      printf("%s, welcome to the game! You have %d attempts to guess the number between %d and %d\n", name, attempts, min, max);
+    }else{
+      printf("Server error\n");
+      return -1;
+    }
+  } else if (buffer[0] == 'u') {
+    printf("Name already exists\n");
+    return -1;
+  }
 
   while (true) {
     printf("Enter your guess or command: ");
     fgets(input, BUFFER_SIZE, stdin);
-    input[strcspn(input, "\n")] = 0; 
+    input[strcspn(input, "\n")] = 0;
 
     if (strcmp(input, "exit") == 0) {
       break;
     }
 
     send(sock, input, strlen(input), 0);
+    printf("Sent: %s\n", input);
 
     valread = read(sock, buffer, BUFFER_SIZE);
-    buffer[valread] = '\0'; 
+    buffer[valread] = '\0';
     printf("Server: %s\n", buffer);
 
-    if (strncmp(buffer, "Victory\n", sizeof("Victory\n")) == 0 ||
-        strncmp(buffer, "Defeat\n", sizeof("Defeat\n")) == 0) {
+    if (strncmp(buffer, "v", 1) == 0 || strncmp(buffer, "d", 1) == 0) {
       break;
     }
   }
@@ -78,3 +110,27 @@ int main() {
   close(sock);
   return 0;
 }
+
+// 1. Клиент отправляет имя
+// 2. Приветственное сообщение с клиентской стороны
+// 3. Сервер принимает конфигурационный файл, в котором указан seed, два числа,
+// которые задают диапазон нижней границы, два числа для верхней границы, порт
+// 4. Только положительные числа
+// Каждые 10 запросов показывать пользователю условие, количество попыток
+// 5. Отправка диапазона клиенту
+// Запуск клиента - ./client -h <host> -p <port> -n <name>
+// Запуск сервера - ./server conffile.txt
+
+// ./a.out
+
+// 10
+// 8080
+
+// 3
+// 10
+
+// 34
+// 55
+
+// 1000
+// 5000
--- lab3/src/server.c
+++ lab3/src/server.c
@@ -1,14 +1,6 @@
-/**
- * @file server.c
- * @brief Server application for a multi-client guessing game over TCP/IP.
- *
- * This server application listens on a specified port for incoming TCP connections.
- * Each connected client is asked to guess a secret number. The server responds to
- * guesses with hints or a notification of correct guess. Supports multiple clients
- * concurrently using non-blocking sockets and select() for multiplexing.
- */
-#include <arpa/inet.h> 
-#include <errno.h>     
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,163 +9,287 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-#define PORT 8080 /**< Port number for the server to listen on. */
-#define MAX_CLIENTS 65536 /**< Maximum number of clients that can connect simultaneously. */
-#define BUFFER_SIZE 1024 /**< Size of the buffer for reading messages from clients. */
-
-/**
- * @struct ClientData
- * @brief Holds information about a connected client.
- *
- * @var ClientData::socket
- * Socket descriptor for the client.
- * @var ClientData::secretNumber
- * The secret number assigned to this client to guess.
- */
+#define PORT 8080
+#define BUFFER_SIZE 256
+#define INITIAL_CLIENTS 1
 
 typedef struct {
-    int socket;
-    int secretNumber;
+  int socket;
+  char name[BUFFER_SIZE];
+  int min;
+  int max;
+  int secretNumber;
+  int attempts;
 } ClientData;
 
-/**
- * @brief Entry point of the server application.
- * 
- * Initializes the server, sets up listening socket and enters the main loop
- * to accept new connections and handle incoming messages from clients.
- *
- * @return int Returns 0 on successful execution.
- */
+typedef struct {
+  int mininit;
+  int maxinit;
+  int minfin;
+  int maxfin;
+  int minattempts;
+  int maxattempts;
+} GameData;
 
-int main() {
-    int server_fd, new_socket, max_sd, sd, activity, valread;
-    ClientData client_data[MAX_CLIENTS];
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
-    fd_set readfds;
+int acceptNewClient(int server_fd, struct sockaddr_in address,
+                    ClientData **client_data, int *client_capacity, GameData *gameData);
+void initializeClientData(ClientData *client_data, int start, int client_capacity);
+int setupServerSocket(int port);
+void handleClientActivity(int sd, ClientData *client_data,
+                          struct sockaddr_in address);
 
-    srand(time(NULL)); 
+#include <stdio.h>
 
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        client_data[i].socket = 0; 
-        client_data[i].secretNumber = 0; 
+void readData(const char *filename, int *seed, int *port, GameData *gameData) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    return;
+  }
+  // TODO: Check data
+  fscanf(file, "%d", seed);
+  fscanf(file, "%d", port);
+  fscanf(file, "%d", &gameData->minattempts);
+  fscanf(file, "%d", &gameData->maxattempts);
+  fscanf(file, "%d", &gameData->mininit);
+  fscanf(file, "%d", &gameData->maxinit);
+  fscanf(file, "%d", &gameData->minfin);
+  fscanf(file, "%d", &gameData->maxfin);
+
+  fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+  GameData gameData;
+  gameData.mininit = 1;
+  gameData.maxinit = 1;
+  gameData.minfin = 100;
+  gameData.maxfin = 100;
+  gameData.minattempts = 8;
+  gameData.maxattempts = 8;
+  int port = PORT;
+  int seed = 1;
+  if (argc != 2) {
+    printf("You can use with config file: %s <path/to/conffile.txt>\n", argv[0]);
+  }else{
+    readData(argv[1], &seed, &port, &gameData);
+  }
+  
+
+  int server_fd, new_socket, max_sd, sd, activity, valread,
+      client_capacity = INITIAL_CLIENTS;
+  ClientData *client_data =
+      (ClientData *)calloc(client_capacity, sizeof(ClientData));
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[BUFFER_SIZE];
+  fd_set readfds;
+
+  srand(seed);
+
+  initializeClientData(client_data, 0, client_capacity);
+
+  server_fd = setupServerSocket(PORT);
+  fcntl(server_fd, F_SETFL, O_NONBLOCK);
+
+  printf("Listener on port %d \n", PORT);
+
+  while (1) {
+    FD_ZERO(&readfds);
+    FD_SET(server_fd, &readfds);
+    max_sd = server_fd;
+
+    for (int i = 0; i < client_capacity; i++) {
+      sd = client_data[i].socket;
+      if (sd > 0) {
+        FD_SET(sd, &readfds);
+      }
+      if (sd > max_sd) {
+        max_sd = sd;
+      }
     }
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+    if ((activity < 0) && (errno != EINTR)) {
+      printf("select error");
     }
 
-    fcntl(server_fd, F_SETFL, O_NONBLOCK);
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+    if (FD_ISSET(server_fd, &readfds)) {
+      acceptNewClient(server_fd, address, &client_data, &client_capacity, &gameData);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    for (int i = 0; i < client_capacity; i++) {
+      if (client_data[i].socket > 0 &&
+          FD_ISSET(client_data[i].socket, &readfds)) {
+        handleClientActivity(client_data[i].socket, &client_data[i], address);
+      }
     }
+  }
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
+  free(client_data);
+  return 0;
+}
+
+int acceptNewClient(int server_fd, struct sockaddr_in address,
+                    ClientData **client_data, int *client_capacity, GameData *gameData) {
+  int new_socket, addrlen = sizeof(address);
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t *)&addrlen)) < 0) {
+    perror("accept");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("New connection, socket fd is %d, ip is : %s, port : %d \n",
+         new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+  fcntl(new_socket, F_SETFL, O_NONBLOCK);
+  char name[BUFFER_SIZE];
+  int valread = read(new_socket, name, BUFFER_SIZE - 1);
+  name[valread] = '\0';
+  int added = 0;
+  for (int i = 0; i < *client_capacity; i++) {
+    printf("Comparing %s with %s\n", (*client_data)[i].name, name);
+    if (strcmp((*client_data)[i].name, name) == 0) {
+      char *message = "u";
+      send(new_socket, message, strlen(message), 0);
+      printf("Username already taken\n");
+      close(new_socket);
+      return -1;
     }
+  }
+  for (int i = 0; i < *client_capacity; i++) {
+    if ((*client_data)[i].socket == 0) {
+      (*client_data)[i].socket = new_socket;
+      (*client_data)[i].min = gameData->mininit + rand() % (gameData->maxinit - gameData->mininit + 1);
+      (*client_data)[i].max = gameData->minfin + rand() % (gameData->maxfin - gameData->minfin + 1);
+      (*client_data)[i].attempts = gameData->minattempts + rand() % (gameData->maxattempts - gameData->minattempts + 1);
+      (*client_data)[i].secretNumber = (*client_data)[i].min + rand() % ((*client_data)[i].max - (*client_data)[i].min + 1);
+      strncpy((*client_data)[i].name, name, valread);
+      printf("Adding to list of sockets as %d with secret number %d, range: %d - %d\n", i, (*client_data)[i].secretNumber, (*client_data)[i].min, (*client_data)[i].max);
+      added = 1;
+      break;
+    }
+  }
 
-    printf("Listener on port %d \n", PORT);
+  if (!added) {
+    *client_capacity *= 2;
+    *client_data = (ClientData *)realloc(*client_data,
+                                         *client_capacity * sizeof(ClientData));
+    initializeClientData(*client_data, *client_capacity / 2, *client_capacity);
+    int i = *client_capacity / 2;
+        (*client_data)[i].socket = new_socket;
+      (*client_data)[i].min = gameData->mininit + rand() % (gameData->maxinit - gameData->mininit + 1);
+      (*client_data)[i].max = gameData->minfin + rand() % (gameData->maxfin - gameData->minfin + 1);
+      (*client_data)[i].attempts = gameData->minattempts + rand() % (gameData->maxattempts - gameData->minattempts + 1);
+      (*client_data)[i].secretNumber = (*client_data)[i].min + rand() % ((*client_data)[i].max - (*client_data)[i].min + 1);
+      strncpy((*client_data)[i].name, name, valread);
+      printf("Resized client data to %d\n", *client_capacity);
+      printf("Adding to list of sockets as %d with secret number %d, range: %d - %d\n", i, (*client_data)[i].secretNumber, (*client_data)[i].min, (*client_data)[i].max);
+  }
 
-    while (1) {
-        FD_ZERO(&readfds);
-        FD_SET(server_fd, &readfds);
-        max_sd = server_fd;
+  // message - min and max to user
+  char *message = (char *)malloc(BUFFER_SIZE);
+  sprintf(message, "h %d %d %d", (*client_data)[*client_capacity - 1].min, (*client_data)[*client_capacity - 1].max, (*client_data)[*client_capacity - 1].attempts);
+  send(new_socket, message, strlen(message), 0);
+  printf("User accepted\n");
 
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            sd = client_data[i].socket;
-            if (sd > 0) {
-                FD_SET(sd, &readfds);
-            }
-            if (sd > max_sd) {
-                max_sd = sd;
-            }
+  return new_socket;
+}
+
+void initializeClientData(ClientData *client_data, int start, int client_capacity) {
+  for (int i = start; i < client_capacity; i++) {
+    client_data[i].socket = 0;
+    client_data[i].name[0] = '\0';
+    client_data[i].min = 0;
+    client_data[i].max = 0;
+    client_data[i].secretNumber = rand() % 100 + 1;
+    client_data[i].attempts = 0;
+  }
+}
+
+int setupServerSocket(int port) {
+  int server_fd;
+  struct sockaddr_in address;
+  int opt = 1;
+
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
+                 sizeof(opt)) < 0) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(port);
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(server_fd, 3) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  return server_fd;
+}
+
+void handleClientActivity(int sd, ClientData *client_data,
+                          struct sockaddr_in address) {
+  int valread, addrlen = sizeof(address);
+  char buffer[BUFFER_SIZE];
+  if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0) {
+    getpeername(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    printf("Host disconnected, ip %s, port %d \n", inet_ntoa(address.sin_addr),
+           ntohs(address.sin_port));
+    close(sd);
+    client_data->socket = 0;
+    client_data->name[0] = '\0';
+  } else {
+    buffer[valread] = '\0';
+    int guessedNumber;
+    char command[20];
+    if (sscanf(buffer, "%s %d", command, &guessedNumber) == 2) {
+      printf("Client %d: %s, Secret: %d, Attempts: %d Min: %d Max: %d\n", client_data->socket, buffer,
+             client_data->secretNumber, client_data->attempts, client_data->min, client_data->max);
+      if (strcmp(command, "g") == 0 && client_data->attempts > 0) {
+        send(sd, client_data->secretNumber > guessedNumber ? "c" : "i", 1, 0);
+        client_data->attempts--;
+      } else if (strcmp(command, "l") == 0 && client_data->attempts > 0) {
+        send(sd, client_data->secretNumber < guessedNumber ? "c" : "i", 1, 0);
+        client_data->attempts--;
+      } else if (strcmp(command, "e") == 0) {
+        if (client_data->secretNumber == guessedNumber) {
+          send(sd, "v", strlen("v"), 0);
+          getpeername(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+          printf("Victory! Host disconnected, ip %s, port %d \n",
+                 inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+          close(sd);
+          client_data->socket = 0;
+          client_data->name[0] = '\0';
+        } else {
+          send(sd, "d", strlen("d"), 0);
+          getpeername(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+          printf("Defeat! Host disconnected, ip %s, port %d \n",
+                 inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+          close(sd);
+          client_data->socket = 0;
+          client_data->name[0] = '\0';
         }
-
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-
-        if ((activity < 0) && (errno != EINTR)) {
-            printf("select error");
-        }
-
-        if (FD_ISSET(server_fd, &readfds)) {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-
-            printf("New connection, socket fd is %d, ip is : %s, port : %d \n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-            fcntl(new_socket, F_SETFL, O_NONBLOCK);
-            char *message = "Welcome to the Guessing Game! \n";
-            send(new_socket, message, strlen(message), 0);
-            printf("Welcome message sent successfully\n");
-
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_data[i].socket == 0) {
-                    client_data[i].socket = new_socket;
-                    client_data[i].secretNumber = rand() % 100 + 1; 
-                    printf("Adding to list of sockets as %d with secret number %d\n", i, client_data[i].secretNumber);
-                    break;
-                }
-            }
-        }
-
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            sd = client_data[i].socket;
-
-            if (FD_ISSET(sd, &readfds)) {
-                if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0) {
-                    getpeername(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-                    printf("Host disconnected, ip %s, port %d \n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-                    close(sd);
-                    client_data[i].socket = 0; 
-                } else {
-                    buffer[valread] = '\0';
-                    int guessedNumber;
-                    char command[20];
-                    if (sscanf(buffer, "number X %s %d?", command, &guessedNumber) == 2) {
-                        if (strcmp(command, "greater") == 0) {
-                            send(sd, client_data[i].secretNumber > guessedNumber ? "Correct\n" : "Incorrect\n", sizeof("Incorrect\n"), 0);
-                        } else if (strcmp(command, "less") == 0) {
-                            send(sd, client_data[i].secretNumber < guessedNumber ? "Correct\n" : "Incorrect\n", sizeof("Incorrect\n"), 0);
-                        } else if (strcmp(command, "equal") == 0) {
-                            if (client_data[i].secretNumber == guessedNumber) {
-                                send(sd, "Victory\n", sizeof("Victory\n"), 0);
-                                printf("Host disconnected, ip %s, port %d \n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-                                close(sd);
-                                client_data[i].socket = 0;
-                            } else {
-                                send(sd, "Defeat\n", sizeof("Defeat\n"), 0);
-                                printf("Host disconnected, ip %s, port %d \n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-                                close(sd);
-                                client_data[i].socket = 0;
-                            }
-                        } else {
-                            send(sd, "incorrect question\n", sizeof("incorrect question\n"), 0);
-                        }
-                    } else {
-                        send(sd, "incorrect question format\n", sizeof("incorrect question format\n"), 0);
-                    }
-                }
-            }
-        }
+      } else if(strcmp(command, "g") == 0 || strcmp(command, "l") == 0){
+        send(sd, "o", 1, 0);
+      }else {
+        send(sd, "q", 1, 0);
+      }
+    } else {
+      send(sd, "f", 1, 0);
     }
-
-    return 0;
+  }
 }
