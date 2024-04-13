4fdf6025b4a16821f32d8f0836976f19033d1a5a
axywe <axywewastaken@gmail.com>
2024-04-13 09:42:41 +0300

    Doxygen

--- lab3/src/client.c
+++ lab3/src/client.c
@@ -1,3 +1,11 @@
+/**
+ * @file client.c
+ * @brief Client for a "Guess the Number" game, interacting with a game server using TCP/IP protocol.
+ *
+ * This client program interacts with a game server to play a simple "Guess the Number" game.
+ * It uses TCP/IP sockets to send guesses and receive responses, and provides a text-based
+ * interface for the user to play the game.
+ */
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdbool.h>
@@ -10,148 +18,193 @@
 #define BUFFER_SIZE 256
 #define PORT 8080
 
-bool parse_and_validate_args(int argc, char *argv[], char **host, int *port, char **name) {
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-h") == 0) {
-            *host = argv[i + 1];
-        } else if (strcmp(argv[i], "-p") == 0) {
-            *port = atoi(argv[i + 1]);
-        } else if (strcmp(argv[i], "-n") == 0) {
-            *name = argv[i + 1];
-        }
-    }
 
-    if (*host == NULL) *host = "127.0.0.1";
-    if (*port == 0) *port = PORT;
-    if (*name == NULL) {
-        printf("Usage: %s -h <host> -p <port> -n <name>\n", argv[0]);
-        return false;
+/**
+ * @brief Parses and validates command line arguments.
+ * @param argc Argument count.
+ * @param argv Argument vector.
+ * @param host Pointer to host address string.
+ * @param port Pointer to port number.
+ * @param name Pointer to username string.
+ * @return True if arguments are valid, false otherwise.
+ */
+bool parse_and_validate_args(int argc, char *argv[], char **host, int *port,
+                             char **name) {
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-h") == 0) {
+      *host = argv[i + 1];
+    } else if (strcmp(argv[i], "-p") == 0) {
+      *port = atoi(argv[i + 1]);
+    } else if (strcmp(argv[i], "-n") == 0) {
+      *name = argv[i + 1];
     }
-    if (*port < 1024 || *port > 65535) {
-        printf("Port must be in range 1024-65535\n");
-        return false;
-    }
-    if (strlen(*name) > 255) {
-        printf("Name must be less than 255 characters\n");
-        return false;
-    }
+  }
 
-    return true;
+  if (*host == NULL)
+    *host = "127.0.0.1";
+  if (*port == 0)
+    *port = PORT;
+  if (*name == NULL) {
+    printf("Usage: %s -h <host> -p <port> -n <name>\n", argv[0]);
+    return false;
+  }
+  if (*port < 1024 || *port > 65535) {
+    printf("Port must be in range 1024-65535\n");
+    return false;
+  }
+  if (strlen(*name) > 255) {
+    printf("Name must be less than 255 characters\n");
+    return false;
+  }
+
+  return true;
 }
 
-char* parse_client_input(const char *input, int *attempts) {
-    static char command[100];
-    int number;
-    char checkChar;
+/**
+ * @brief Parses the client's textual input into game-specific commands.
+ * @param input User's textual input string.
+ * @return A string containing a game command or error indicator.
+ */
+char *parse_client_input(const char *input) {
+  static char command[100];
+  int number;
+  char checkChar;
 
-    if (sscanf(input, "greater than %d%c", &number, &checkChar) == 1) {
-        sprintf(command, "g %d", number);
-        if (*attempts > 0) (*attempts)--;
-    } else if (sscanf(input, "less than %d%c", &number, &checkChar) == 1) {
-        sprintf(command, "l %d", number);
-        if (*attempts > 0) (*attempts)--;
-    } else if (sscanf(input, "equal %d%c", &number, &checkChar) == 1) {
-        sprintf(command, "e %d", number);
-    } else if (strcmp(input, "exit") == 0) {
-        strcpy(command, "exit");
-    } else {
-        strcpy(command, "i");
-    }
-    if(number < 0 || strlen(input) > 22) {
-      strcpy(command, "i");
-    }
-    return command;
+  if (sscanf(input, "greater than %d%c", &number, &checkChar) == 1) {
+    sprintf(command, "g %d", number);
+  } else if (sscanf(input, "less than %d%c", &number, &checkChar) == 1) {
+    sprintf(command, "l %d", number);
+  } else if (sscanf(input, "equal %d%c", &number, &checkChar) == 1) {
+    sprintf(command, "e %d", number);
+  } else if (strcmp(input, "exit") == 0) {
+    strcpy(command, "exit");
+  } else {
+    strcpy(command, "i");
+  }
+  if (number < 0 || strlen(input) > 22) {
+    strcpy(command, "i");
+  }
+  return command;
 }
 
-char* parse_server_response(char response) {
-    switch (response) {
-        case 'c':
-            return "Correct guess.";
-        case 'i':
-            return "Incorrect guess.";
-        case 'v':
-            return "Victory!";
-        case 'd':
-            return "Defeat.";
-        case 'f':
-            return "Incorrect question format.";
-        case 'q':
-            return "Invalid question.";
-        case 'o':
-            return "Out of attempts.";
-        default:
-            return "Unknown response.";
-    }
+/**
+ * @brief Parses the server's single character responses into human-readable strings.
+ * @param response Character response from server.
+ * @return Human-readable string describing the server's response.
+ */
+char *parse_server_response(char response) {
+  switch (response) {
+  case 'c':
+    return "Correct guess.";
+  case 'i':
+    return "Incorrect guess.";
+  case 'v':
+    return "Victory!";
+  case 'd':
+    return "Defeat.";
+  case 'f':
+    return "Incorrect question format.";
+  case 'q':
+    return "Invalid question.";
+  case 'o':
+    return "Out of attempts.";
+  default:
+    return "Unknown response.";
+  }
 }
 
-
-void display_game_state(const char *username, int attempts, int min, int max, const char *last_input, const char *last_response) {
-    printf("\033[2J\033[H"); // Clear screen and move cursor to home position
-    printf("\033[1;31mWelcome to the number guessing game!\033[0m\n");
-    printf("\033[1;30mCommands:\033[0m\n");
-    printf("\033[1;30mgreater than <positive integer>\033[0m\n");
-    printf("\033[1;30mlower than <positive integer>\033[0m\n");
-    printf("\033[1;30mequal <positive integer>\033[0m\n");
-    printf("\033[1;34mUsername: %.10s\033[0m\n", username);
-    printf("\033[1;32mAttempts remaining: %3d\033[0m\n", attempts);
-    printf("\033[1;35mRange: %d - %d\033[0m\n", min, max);
-    printf("\033[1;33mLast Input: %s\033[0m\n", last_input);
-    printf("\033[1;33mLast Server Response: %s\033[0m\n", last_response);
-    printf("\033[1;33m----------------------------------------\033[0m\n");
+/**
+ * @brief Displays the current state of the game including user inputs and server responses.
+ * @param username The user's name.
+ * @param attempts The number of attempts remaining.
+ * @param min The minimum number in the guessing range.
+ * @param max The maximum number in the guessing range.
+ * @param last_input The last input given by the user.
+ * @param last_response The last response received from the server.
+ */
+void display_game_state(const char *username, int attempts, int min, int max,
+                        const char *last_input, const char *last_response) {
+  printf("\033[2J\033[H"); 
+  printf("\033[1;31mWelcome to the number guessing game!\033[0m\n");
+  printf("\033[1;30mCommands:\033[0m\n");
+  printf("\033[1;30mgreater than <positive integer>\033[0m\n");
+  printf("\033[1;30mlower than <positive integer>\033[0m\n");
+  printf("\033[1;30mequal <positive integer>\033[0m\n");
+  printf("\033[1;34mUsername: %.10s\033[0m\n", username);
+  printf("\033[1;32mAttempts remaining: %3d\033[0m\n", attempts);
+  printf("\033[1;35mRange: %d - %d\033[0m\n", min, max);
+  printf("\033[1;33mLast Input: %s\033[0m\n", last_input);
+  printf("\033[1;33mLast Server Response: %s\033[0m\n", last_response);
+  printf("\033[1;33m----------------------------------------\033[0m\n");
+  printf("\033[1;36mEnter your guess or command: \033[0m");
 }
 
+/**
+ * @brief Main loop for the game, handling input, output, and communication with the server.
+ * @param sock Socket descriptor connected to the server.
+ * @param attempts Number of attempts given to guess the number.
+ * @param min Minimum value in the number range.
+ * @param max Maximum value in the number range.
+ * @param name User's name.
+ */
 void game_loop(int sock, int attempts, int min, int max, char *name[]) {
-    char buffer[BUFFER_SIZE];
-    char input[BUFFER_SIZE];
-    char last_input[BUFFER_SIZE] = "";
-    char last_response[BUFFER_SIZE] = "";
+  char buffer[BUFFER_SIZE];
+  char input[BUFFER_SIZE];
+  char last_input[BUFFER_SIZE] = "";
+  char last_response[BUFFER_SIZE] = "";
 
-    while (true) {
-        display_game_state(*name, attempts, min, max, last_input, last_response);
-        printf("\033[1;36mEnter your guess or command: \033[0m");
-        fgets(input, BUFFER_SIZE, stdin);
-        input[strcspn(input, "\n")] = 0;
-        char* parsed_input = parse_client_input(input, &attempts);
-        if (strcmp(parsed_input, "exit") == 0) {
-            break;
-        }
-
-        strcpy(last_input, input);
-        if (strcmp(parsed_input, "i") == 0) {
-            sprintf(last_response, "Invalid input");
-            continue;
-        }
-
-        send(sock, parsed_input, strlen(parsed_input), 0);
-
-        int valread = read(sock, buffer, 1);
-        if (valread == 0 || valread == -1) {
-            printf("Server disconnected\n");
-            break;
-        }
-        strcpy(last_response, parse_server_response(buffer[0]));
-
-        if (strncmp(buffer, "v", 1) == 0 || strncmp(buffer, "d", 1) == 0) {
-            printf("%s\n", parse_server_response(buffer[0])); 
-            break;
-        }
-
+  while (true) {
+    display_game_state(*name, attempts, min, max, last_input, last_response);
+    fgets(input, BUFFER_SIZE, stdin);
+    input[strcspn(input, "\n")] = 0;
+    char *parsed_input = parse_client_input(input);
+    if (strcmp(parsed_input, "exit") == 0) {
+      break;
     }
+
+    strcpy(last_input, input);
+    if (strcmp(parsed_input, "i") == 0) {
+      sprintf(last_response, "Invalid input");
+      continue;
+    }
+
+    send(sock, parsed_input, strlen(parsed_input), 0);
+
+    int valread = recv(sock, buffer, 1, 0);
+    if (valread == 0 || valread == -1) {
+      printf("Server disconnected\n");
+      break;
+    }
+    if (buffer[0] == 'c' || buffer[0] == 'i') {
+      attempts--;
+    }
+    strcpy(last_response, parse_server_response(buffer[0]));
+
+    if (strncmp(buffer, "v", 1) == 0 || strncmp(buffer, "d", 1) == 0) {
+      printf("%s\n", parse_server_response(buffer[0]));
+      break;
+    }
+  }
 }
 
-
+/**
+ * @brief Main function for the client application.
+ * @param argc Number of arguments.
+ * @param argv Array of argument strings.
+ * @return 0 on normal exit, or -1 on error.
+ */
 int main(int argc, char *argv[]) {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
+  int sock = 0;
+  struct sockaddr_in serv_addr;
+  char buffer[BUFFER_SIZE] = {0};
 
-    char *host = NULL;
-    int port = 0;
-    char *name = NULL;
+  char *host = NULL;
+  int port = 0;
+  char *name = NULL;
 
-    if (!parse_and_validate_args(argc, argv, &host, &port, &name)) {
-        return -1;
-    }
+  if (!parse_and_validate_args(argc, argv, &host, &port, &name)) {
+    return -1;
+  }
 
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Socket creation error \n");
@@ -171,14 +224,14 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  send(sock, name, strlen(name)+1, 0);
-  int valread = read(sock, buffer, BUFFER_SIZE);
+  send(sock, name, strlen(name) + 1, 0);
+  int valread = recv(sock, buffer, BUFFER_SIZE, 0);
   int min, max, attempts;
   if (valread == 0) {
     printf("Server disconnected\n");
     return -1;
   } else if (buffer[0] == 'h') {
-    if(sscanf(buffer, "h %d %d %d", &min, &max, &attempts) != 3){
+    if (sscanf(buffer, "h %d %d %d", &min, &max, &attempts) != 3) {
       printf("Server error\n");
       return -1;
     }
--- lab3/src/server.c
+++ lab3/src/server.c
@@ -1,3 +1,12 @@
+/**
+ * @file server.c
+ * @brief Server for a "Guess the Number" game, handling multiple clients with a TCP/IP protocol.
+ *
+ * This server program implements the game logic for a simple "Guess the Number" game, which
+ * can be played by multiple clients simultaneously. It uses TCP/IP sockets to communicate with
+ * clients, and handles each client's game state independently.
+ */
+
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -14,6 +23,23 @@
 #define BUFFER_SIZE 256
 #define INITIAL_CLIENTS 1
 
+/**
+ * @struct ClientData
+ * @brief Holds individual client game data.
+ *
+ * @var ClientData::socket
+ * The socket descriptor for the client connection.
+ * @var ClientData::name
+ * The client's username.
+ * @var ClientData::min
+ * The minimum number in the range for guessing.
+ * @var ClientData::max
+ * The maximum number in the range for guessing.
+ * @var ClientData::secretNumber
+ * The secret number the client needs to guess.
+ * @var ClientData::attempts
+ * The number of attempts the client has to guess the number.
+ */
 typedef struct {
   int socket;
   char name[BUFFER_SIZE];
@@ -23,6 +49,24 @@ typedef struct {
   int attempts;
 } ClientData;
 
+
+/**
+ * @struct GameData
+ * @brief Holds configuration data for the game.
+ *
+ * @var GameData::mininit
+ * Initial minimum range for the secret number.
+ * @var GameData::maxinit
+ * Initial maximum range for the secret number.
+ * @var GameData::minfin
+ * Final minimum range after dynamic adjustment.
+ * @var GameData::maxfin
+ * Final maximum range after dynamic adjustment.
+ * @var GameData::minattempts
+ * Minimum number of attempts allowed per game.
+ * @var GameData::maxattempts
+ * Maximum number of attempts allowed per game.
+ */
 typedef struct {
   int mininit;
   int maxinit;
@@ -32,47 +76,61 @@ typedef struct {
   int maxattempts;
 } GameData;
 
+/**
+ * @brief Accepts a new client, sets up its game data, and registers it.
+ * @param server_fd Socket descriptor for the server.
+ * @param address Struct containing address information of the server.
+ * @param client_data Pointer to the array of client data structures.
+ * @param client_capacity Pointer to the current capacity of the client data array.
+ * @param gameData Pointer to the game configuration data.
+ * @return The socket descriptor of the newly accepted client or -1 on failure.
+ */
 int acceptNewClient(int server_fd, struct sockaddr_in address,
-                    ClientData **client_data, int *client_capacity, GameData *gameData);
-void initializeClientData(ClientData *client_data, int start, int client_capacity);
+                    ClientData **client_data, int *client_capacity,
+                    GameData *gameData);
+            
+/**
+ * @brief Initializes the client data for new or expanding client arrays.
+ * @param client_data Pointer to the client data array.
+ * @param start Index from which to start initializing.
+ * @param client_capacity The total capacity of the client data array.
+ */
+void initializeClientData(ClientData *client_data, int start,
+                          int client_capacity);
+
+/**
+ * @brief Sets up the server socket, binds, and listens.
+ * @param port The port on which the server should listen.
+ * @return The socket descriptor for the server.
+ */
 int setupServerSocket(int port);
+
+/**
+ * @brief Handles the activity for a specific client.
+ * @param sd The socket descriptor of the client.
+ * @param client_data Pointer to the client's data structure.
+ * @param address Struct containing the client's address information.
+ */
 void handleClientActivity(int sd, ClientData *client_data,
                           struct sockaddr_in address);
 
-int readData(const char *filename, int *seed, int *port, GameData *gameData) {
-  FILE *file = fopen(filename, "r");
-  if (file == NULL) {
-    printf("File not found\n");
-    return -1;
-  }
-  fscanf(file, "%d", seed);
-  fscanf(file, "%d", port);
-  fscanf(file, "%d", &gameData->minattempts);
-  fscanf(file, "%d", &gameData->maxattempts);
-  fscanf(file, "%d", &gameData->mininit);
-  fscanf(file, "%d", &gameData->maxinit);
-  fscanf(file, "%d", &gameData->minfin);
-  fscanf(file, "%d", &gameData->maxfin);
+/**
+ * @brief Reads game configuration data from a file.
+ * @param filename Path to the configuration file.
+ * @param seed Pointer to the seed value for the random number generator.
+ * @param port Pointer to the port number on which the server will listen.
+ * @param gameData Pointer to the structure where game data will be stored.
+ * @return 0 on success, -1 on failure.
+ */
+int readData(const char *filename, int *seed, int *port, GameData *gameData);
 
 
-  fclose(file);
-  if(*port < 1024 || *port > 65535){
-    printf("Port must be in range 1024-65535\n");
-    return -1;
-  }
-
-  if(gameData->minattempts < 1 || gameData->maxattempts < 1 || gameData->mininit < 1 || gameData->maxinit < 1 || gameData->minfin < 1 || gameData->maxfin < 1){
-    printf("All values must be greater than 0\n");
-    return -1;
-  }
-
-  if(gameData->minattempts > gameData->maxattempts || gameData->mininit > gameData->maxinit || gameData->minfin > gameData->maxfin){
-    printf("Min values must be less than max values\n");
-    return -1;
-  }
-  return 0;
-}
-
+/**
+ * @brief Main function to execute the server logic.
+ * @param argc Number of arguments.
+ * @param argv Array of argument strings.
+ * @return 0 on normal exit, or -1 on error.
+ */
 int main(int argc, char *argv[]) {
   GameData gameData;
   gameData.mininit = 1;
@@ -84,14 +142,14 @@ int main(int argc, char *argv[]) {
   int port = PORT;
   int seed = 1;
   if (argc != 2) {
-    printf("You can use with config file: %s <path/to/conffile.txt>\n", argv[0]);
-  }else{
+    printf("You can use with config file: %s <path/to/conffile.txt>\n",
+           argv[0]);
+  } else {
     int result = readData(argv[1], &seed, &port, &gameData);
-    if(result == -1){
+    if (result == -1) {
       return -1;
     }
   }
-  
 
   int server_fd, new_socket, max_sd, sd, activity, valread,
       client_capacity = INITIAL_CLIENTS;
@@ -134,7 +192,8 @@ int main(int argc, char *argv[]) {
     }
 
     if (FD_ISSET(server_fd, &readfds)) {
-      acceptNewClient(server_fd, address, &client_data, &client_capacity, &gameData);
+      acceptNewClient(server_fd, address, &client_data, &client_capacity,
+                      &gameData);
     }
 
     for (int i = 0; i < client_capacity; i++) {
@@ -150,7 +209,8 @@ int main(int argc, char *argv[]) {
 }
 
 int acceptNewClient(int server_fd, struct sockaddr_in address,
-                    ClientData **client_data, int *client_capacity, GameData *gameData) {
+                    ClientData **client_data, int *client_capacity,
+                    GameData *gameData) {
   int new_socket, addrlen = sizeof(address);
   if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                            (socklen_t *)&addrlen)) < 0) {
@@ -171,7 +231,7 @@ int acceptNewClient(int server_fd, struct sockaddr_in address,
   FD_ZERO(&readfds);
   FD_SET(new_socket, &readfds);
 
-  tv.tv_sec = 5;  
+  tv.tv_sec = 5;
   tv.tv_usec = 0;
 
   retval = select(new_socket + 1, &readfds, NULL, NULL, &tv);
@@ -181,7 +241,7 @@ int acceptNewClient(int server_fd, struct sockaddr_in address,
     close(new_socket);
     return -1;
   } else if (retval) {
-    valread = read(new_socket, name, BUFFER_SIZE - 1);
+    valread = recv(new_socket, name, BUFFER_SIZE - 1, 0);
     if (valread > 0) {
       name[valread] = '\0';
       printf("Valread: %d, Name: %s\n", valread, name);
@@ -214,12 +274,22 @@ int acceptNewClient(int server_fd, struct sockaddr_in address,
   for (int i = 0; i < *client_capacity; i++) {
     if ((*client_data)[i].socket == 0) {
       (*client_data)[i].socket = new_socket;
-      (*client_data)[i].min = gameData->mininit + rand() % (gameData->maxinit - gameData->mininit + 1);
-      (*client_data)[i].max = gameData->minfin + rand() % (gameData->maxfin - gameData->minfin + 1);
-      (*client_data)[i].attempts = gameData->minattempts + rand() % (gameData->maxattempts - gameData->minattempts + 1);
-      (*client_data)[i].secretNumber = (*client_data)[i].min + rand() % ((*client_data)[i].max - (*client_data)[i].min + 1);
+      (*client_data)[i].min =
+          gameData->mininit +
+          rand() % (gameData->maxinit - gameData->mininit + 1);
+      (*client_data)[i].max =
+          gameData->minfin + rand() % (gameData->maxfin - gameData->minfin + 1);
+      (*client_data)[i].attempts =
+          gameData->minattempts +
+          rand() % (gameData->maxattempts - gameData->minattempts + 1);
+      (*client_data)[i].secretNumber =
+          (*client_data)[i].min +
+          rand() % ((*client_data)[i].max - (*client_data)[i].min + 1);
       strncpy((*client_data)[i].name, name, valread);
-      printf("Adding to list of sockets as %d with secret number %d, range: %d - %d\n", i, (*client_data)[i].secretNumber, (*client_data)[i].min, (*client_data)[i].max);
+      printf("Adding to list of sockets as %d with secret number %d, range: %d "
+             "- %d\n",
+             i, (*client_data)[i].secretNumber, (*client_data)[i].min,
+             (*client_data)[i].max);
       added = i;
       break;
     }
@@ -231,27 +301,40 @@ int acceptNewClient(int server_fd, struct sockaddr_in address,
                                          *client_capacity * sizeof(ClientData));
     initializeClientData(*client_data, *client_capacity / 2, *client_capacity);
     int i = *client_capacity / 2;
-        (*client_data)[i].socket = new_socket;
-      (*client_data)[i].min = gameData->mininit + rand() % (gameData->maxinit - gameData->mininit + 1);
-      (*client_data)[i].max = gameData->minfin + rand() % (gameData->maxfin - gameData->minfin + 1);
-      (*client_data)[i].attempts = gameData->minattempts + rand() % (gameData->maxattempts - gameData->minattempts + 1);
-      (*client_data)[i].secretNumber = (*client_data)[i].min + rand() % ((*client_data)[i].max - (*client_data)[i].min + 1);
-      strncpy((*client_data)[i].name, name, valread);
-      printf("Resized client data to %d\n", *client_capacity);
-      printf("Adding to list of sockets as %d with secret number %d, range: %d - %d\n", i, (*client_data)[i].secretNumber, (*client_data)[i].min, (*client_data)[i].max);
-      added = i;
+    (*client_data)[i].socket = new_socket;
+    (*client_data)[i].min =
+        gameData->mininit +
+        rand() % (gameData->maxinit - gameData->mininit + 1);
+    (*client_data)[i].max =
+        gameData->minfin + rand() % (gameData->maxfin - gameData->minfin + 1);
+    (*client_data)[i].attempts =
+        gameData->minattempts +
+        rand() % (gameData->maxattempts - gameData->minattempts + 1);
+    (*client_data)[i].secretNumber =
+        (*client_data)[i].min +
+        rand() % ((*client_data)[i].max - (*client_data)[i].min + 1);
+    strncpy((*client_data)[i].name, name, valread);
+    printf("Resized client data to %d\n", *client_capacity);
+    printf("Adding to list of sockets as %d with secret number %d, range: %d - "
+           "%d\n",
+           i, (*client_data)[i].secretNumber, (*client_data)[i].min,
+           (*client_data)[i].max);
+    added = i;
   }
 
   char *message = (char *)malloc(BUFFER_SIZE);
-  sprintf(message, "h %d %d %d", (*client_data)[added].min, (*client_data)[added].max, (*client_data)[added].attempts);
+  sprintf(message, "h %d %d %d", (*client_data)[added].min,
+          (*client_data)[added].max, (*client_data)[added].attempts);
   send(new_socket, message, strlen(message), 0);
-  printf("User accepted, send message: %s, %d, %d\n", message, *client_capacity - 1, added);
+  printf("User accepted, send message: %s, %d, %d\n", message,
+         *client_capacity - 1, added);
   free(message);
 
   return new_socket;
 }
 
-void initializeClientData(ClientData *client_data, int start, int client_capacity) {
+void initializeClientData(ClientData *client_data, int start,
+                          int client_capacity) {
   for (int i = start; i < client_capacity; i++) {
     client_data[i].socket = 0;
     client_data[i].name[0] = '\0';
@@ -299,7 +382,7 @@ void handleClientActivity(int sd, ClientData *client_data,
                           struct sockaddr_in address) {
   int valread, addrlen = sizeof(address);
   char buffer[BUFFER_SIZE];
-  if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0) {
+  if ((valread = recv(sd, buffer, BUFFER_SIZE, 0)) == 0) {
     getpeername(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
     printf("Host disconnected, ip %s, port %d \n", inet_ntoa(address.sin_addr),
            ntohs(address.sin_port));
@@ -311,8 +394,9 @@ void handleClientActivity(int sd, ClientData *client_data,
     int guessedNumber;
     char command[20];
     if (sscanf(buffer, "%s %d", command, &guessedNumber) == 2) {
-      printf("Client %d: %s, Secret: %d, Attempts: %d Min: %d Max: %d\n", client_data->socket, buffer,
-             client_data->secretNumber, client_data->attempts, client_data->min, client_data->max);
+      printf("Client %d: %s, Secret: %d, Attempts: %d Min: %d Max: %d\n",
+             client_data->socket, buffer, client_data->secretNumber,
+             client_data->attempts, client_data->min, client_data->max);
       if (strcmp(command, "g") == 0 && client_data->attempts > 0) {
         send(sd, client_data->secretNumber > guessedNumber ? "c" : "i", 1, 0);
         client_data->attempts--;
@@ -337,13 +421,51 @@ void handleClientActivity(int sd, ClientData *client_data,
           client_data->socket = 0;
           client_data->name[0] = '\0';
         }
-      } else if(strcmp(command, "g") == 0 || strcmp(command, "l") == 0){
+      } else if (strcmp(command, "g") == 0 || strcmp(command, "l") == 0) {
         send(sd, "o", 1, 0);
-      }else {
+      } else {
         send(sd, "q", 1, 0);
       }
     } else {
       send(sd, "f", 1, 0);
     }
   }
+}
+
+
+int readData(const char *filename, int *seed, int *port, GameData *gameData) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    printf("File not found\n");
+    return -1;
+  }
+  fscanf(file, "%d", seed);
+  fscanf(file, "%d", port);
+  fscanf(file, "%d", &gameData->minattempts);
+  fscanf(file, "%d", &gameData->maxattempts);
+  fscanf(file, "%d", &gameData->mininit);
+  fscanf(file, "%d", &gameData->maxinit);
+  fscanf(file, "%d", &gameData->minfin);
+  fscanf(file, "%d", &gameData->maxfin);
+
+  fclose(file);
+  if (*port < 1024 || *port > 65535) {
+    printf("Port must be in range 1024-65535\n");
+    return -1;
+  }
+
+  if (gameData->minattempts < 1 || gameData->maxattempts < 1 ||
+      gameData->mininit < 1 || gameData->maxinit < 1 || gameData->minfin < 1 ||
+      gameData->maxfin < 1) {
+    printf("All values must be greater than 0\n");
+    return -1;
+  }
+
+  if (gameData->minattempts > gameData->maxattempts ||
+      gameData->mininit > gameData->maxinit ||
+      gameData->minfin > gameData->maxfin) {
+    printf("Min values must be less than max values\n");
+    return -1;
+  }
+  return 0;
 }
